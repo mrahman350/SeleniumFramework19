ff536b008562b019ec9ac94d95f21aab795bc4c7
Refat <rahmanm7119@gmail.com>
2019-03-24 13:34:11 -0400

    CreatedTestNG

--- src/test1/PracticewithJakya.java
+++ src/test1/PracticewithJakya.java
@@ -3,10 +3,12 @@ package test1;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class PracticewithJakya {
 
-	public static void main(String[] args) {
+	 @Test
+	 public void practicewithJkya (){
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\refat\\Documents\\TalentTek\\GitHub\\SeleniumFramework19\\Drivers//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.facebook.com");
